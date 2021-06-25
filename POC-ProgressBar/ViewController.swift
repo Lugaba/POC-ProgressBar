0595a6a738414de981138f3490cb796812f239c9
Luca Hummel <luca.hummel12@gmail.com>
2021-06-25 14:41:14 -0300

    Done

--- POC-ProgressBar/ViewController.swift
+++ POC-ProgressBar/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    
+    var progresso = Progress(totalUnitCount: 10)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        progressView.transform = progressView.transform.scaledBy(x: 1, y: 5)
     }
 
+    @IBAction func didTapStartProgress(_ sender: Any) {
+        if progresso.isFinished { // reiniciando o progresso
+            progressView.progress = 0
+            progresso = Progress(totalUnitCount: 10)
+        } else {
+            self.progresso.completedUnitCount += 1
 
+            let progressFloat = Float(self.progresso.fractionCompleted)
+            self.progressView.setProgress(progressFloat, animated: true)
+        }
+        
+//        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+//            guard self.progresso.isFinished == false else {
+//                timer.invalidate()
+//                return
+//            }
+//
+//            self.progresso.completedUnitCount += 1
+//
+//            let progressFloat = Float(self.progresso.fractionCompleted)
+//            self.progressView.setProgress(progressFloat, animated: true)
+//
+//        }
+    }
+    
 }
 
